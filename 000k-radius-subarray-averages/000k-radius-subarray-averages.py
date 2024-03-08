62c20cd6b84b59cd91f7a2e934335e0f8dfdaf82
Hyojoon Park <hpar9758@naver.com>
2024-03-08 20:48:30 +1100

    Time: 1198 ms, Memory: 34.5 MB - LeetHub

--- 000k-radius-subarray-averages/000k-radius-subarray-averages.py
+++ 000k-radius-subarray-averages/000k-radius-subarray-averages.py
@@ -1,16 +1,14 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
         
+        n = len(nums)
         prefix = [nums[0]]
-        for i in range(1, len(nums)):
+        for i in range(1, n):
             prefix.append(nums[i] + prefix[-1])
         
-        answer = [-1] * len(nums)
-        for i in range(k, len(nums)-k):
-            if i - k == 0:
-                answer[i] = prefix[i+k] // (2*k+1)
-            else:
-                answer[i] = (prefix[i+k] - prefix[i-k-1]) // (2*k+1)
+        answer = [-1] * n
+        for i in range(k, n-k):
+            answer[i] = (prefix[i+k] - prefix[i-k] + nums[i-k]) // (2*k+1)
         
         return answer
         
