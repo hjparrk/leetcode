2b702031aaeb0c9ddf3cc7f6e177958462a3f244
Hyojoon Park <hpar9758@naver.com>
2024-03-29 01:25:29 +1100

    Time: 47 ms, Memory: 16.9 MB - LeetHub

--- largest-unique-number/largest-unique-number.py
+++ largest-unique-number/largest-unique-number.py
@@ -3,9 +3,22 @@ from collections import defaultdict
 class Solution:
     def largestUniqueNumber(self, nums: List[int]) -> int:
         
+        ans = -1
         count = defaultdict(int)
         for num in nums:
             count[num] += 1
         l = [k for k in count if count[k] == 1]
         return max(l) if len(l) else -1
+    
+#         s1 = set()
+#         s2 = set()
+
+#         for n in nums:
+#             if n not in s1 and n not in s2:
+#                 s1.add(n)
+#             elif n not in s2:
+#                 s1.remove(n)
+#                 s2.add(n)
+        
+#         return max(s1) if s1 else -1
                 
