60a7f9d50b24d922320b52e2f9760ef96f801442
Hyojoon Park <hpar9758@naver.com>
2024-03-29 01:44:25 +1100

    Time: 47 ms, Memory: 16.9 MB - LeetHub

--- maximum-number-of-balloons/maximum-number-of-balloons.py
+++ maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -11,7 +11,7 @@ class Solution:
         if len(chars & count.keys()) != 5:
             return ans
         
-        ans = min(count['b'], count['a'], count['l'] // 2, count['o'] // 2, count['n'] )
+        ans = min(count['b'], count['a'], count['l'] // 2, count['o'] // 2, count['n'])
         return ans
         
         
