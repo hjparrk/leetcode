188e04eba8d8f968ac13e3f877e5fb77b2bc3160
Hyojoon Park <hpar9758@naver.com>
2024-03-29 01:44:01 +1100

    Time: 32 ms, Memory: 16.7 MB - LeetHub

---
+++ maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        
+        ans = 0
+        chars = set("balloon")
+        count = Counter(text)
+        
+        # set 'AND' operation
+        if len(chars & count.keys()) != 5:
+            return ans
+        
+        ans = min(count['b'], count['a'],count['l'] // 2,count['o'] // 2,count['n'] )
+        return ans
+        
+        
