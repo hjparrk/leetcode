437c08f7abfcee90ffb7ea71777040c476d3b1f8
Hyojoon Park <hpar9758@naver.com>
2024-04-04 01:11:18 +1100

    Time: 55 ms, Memory: 16.8 MB - LeetHub

---
+++ ransom-note/ransom-note.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        count = defaultdict(int)
+        for c in magazine:
+            count[c] += 1
+            
+        for c in ransomNote:
+            if c in magazine and count[c] > 0:
+                count[c] += -1
+            else:
+                return False
+        return True
+                
+        
+        
