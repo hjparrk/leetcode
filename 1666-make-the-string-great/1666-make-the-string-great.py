d6e88d07d1eccae24989fb2d6ecddf3cb03aa274
Hyojoon Park <hpar9758@naver.com>
2024-04-05 00:59:30 +1100

    Time: 44 ms (17.78%), Space: 16.5 MB (46.76%) - LeetHub

---
+++ 1666-make-the-string-great/1666-make-the-string-great.py
@@ -0,0 +1,14 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if not stack:
+                stack.append(c)
+            elif c.islower() and stack[-1] == c.upper():
+                stack.pop()
+            elif c.isupper() and stack[-1] == c.lower():
+                stack.pop()
+            else:
+                stack.append(c)
+
+        return "".join(stack)
