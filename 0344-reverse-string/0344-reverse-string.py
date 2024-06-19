8d8f012c2a9e21a3d8f2b7eb4977ee873d0627ae
Hyojoon Park <hpar9758@naver.com>
2024-06-19 20:14:12 +1000

    Time: 174 ms (23.3%), Space: 21.1 MB (19.7%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,14 +3,8 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left = 0
-        right = len(s) - 1
-
+        left, right = 0, len(s) - 1
         while left < right:
-            tmp = s[left]
-            s[left] = s[right]
-            s[right] = tmp
-
-            left += 1
-            right -= 1
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
         
