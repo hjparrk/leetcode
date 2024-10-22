047acea48febb5915b970311296bfb7dd43bafa2
Hyojoon Park <hpar9758@naver.com>
2024-10-22 19:42:36 +1100

    Time: 0 ms (100%), Space: 17.1 MB (27.68%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        
+        seen = {0}
+        def dfs(node):
+            for neighbor in rooms[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(neighbor)
+
+        dfs(0)
+        return len(rooms) == len(seen)
