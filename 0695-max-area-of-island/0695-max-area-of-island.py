d5fffa10dcc45c75d09dd7cf2c0b82d42f572bc4
Hyojoon Park <hpar9758@naver.com>
2024-10-22 23:38:03 +1100

    Time: 34 ms (96.87%), Space: 18.9 MB (5.47%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,46 +2,31 @@ from collections import defaultdict
 
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
+        rows, cols = len(grid), len(grid[0])
 
-        graph = defaultdict(list)
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == 1:
-                    node = (row,col)
-                    up, down, left, right = (row-1,col), (row+1,col), (row,col-1), (row,col+1)
-
-                    if row - 1 >= 0 and grid[row-1][col] == 1:
-                        graph[node].append(up)
-
-                    if row + 1 < m and grid[row+1][col] == 1:
-                        graph[node].append(down)
-
-                    if col - 1 >= 0 and grid[row][col-1]:
-                        graph[node].append(left)
-
-                    if col + 1 < n and grid[row][col+1]:
-                        graph[node].append(right)
-
-        seen = set()
-        def dfs(node):
-            area = 0
-            for neighbor in graph[node]:
-                if neighbor not in seen:
-                    seen.add(neighbor)
-                    area += 1 + dfs(neighbor)
-            return area
+        def is_valid(row, col):
+            return 0 <= row < rows and 0 <= col <cols
         
-        ans = 0
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == 1:
-                    area = 0
-                    if graph[(row,col)]:
-                        area = dfs((row,col))
-                    else:
-                        area = 1
-                    ans = max(ans, area)
+        seen = set()
+        directions = [[1,0],[-1,0],[0,-1],[0,1]] # up, down, left, right
+        def dfs(row,col):
+            area = 1
+            seen.add((row,col))
+            for dx, dy in directions:
+                new_row, new_col = row + dx, col + dy
+                if is_valid(new_row, new_col) and (new_row, new_col) not in seen and grid[new_row][new_col] == 1:
+                    area += dfs(new_row, new_col)
+            return area
 
-        return ans
+        max_area = 0
+        for row in range(rows):
+            for col in range(cols):
+                if (row,col) not in seen and grid[row][col] == 1:
+                    area = dfs(row,col)
+                    if area == 0:
+                        max_area = max(max_area, 1)
+                    else:
+                        max_area = max(max_area, area)
+        return max_area
+        
         
