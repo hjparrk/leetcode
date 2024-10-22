47bba95eaecb54552537f526ae4286abb07afa79
Hyojoon Park <hpar9758@naver.com>
2024-10-22 23:42:45 +1100

    Time: 37 ms (96.69%), Space: 18.9 MB (5.47%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -14,7 +14,7 @@ class Solution:
             seen.add((row,col))
             for dx, dy in directions:
                 new_row, new_col = row + dx, col + dy
-                if is_valid(new_row, new_col) and (new_row, new_col) not in seen and grid[new_row][new_col] == 1:
+                if is_valid(new_row, new_col) and grid[new_row][new_col] == 1 and (new_row, new_col) not in seen:
                     area += dfs(new_row, new_col)
             return area
 
