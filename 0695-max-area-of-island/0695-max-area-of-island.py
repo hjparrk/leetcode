fab83b80ed400e7ba580382fba50e5f529db39a7
Hyojoon Park <hpar9758@naver.com>
2024-10-22 23:43:26 +1100

    Time: 39 ms (96.64%), Space: 18.8 MB (5.47%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -14,7 +14,7 @@ class Solution:
             seen.add((row,col))
             for dx, dy in directions:
                 new_row, new_col = row + dx, col + dy
-                if is_valid(new_row, new_col) and grid[new_row][new_col] == 1 and (new_row, new_col) not in seen:
+                if is_valid(new_row, new_col) and (new_row, new_col) not in seen and grid[new_row][new_col] == 1:
                     area += dfs(new_row, new_col)
             return area
 
@@ -22,7 +22,7 @@ class Solution:
         for row in range(rows):
             for col in range(cols):
                 if (row,col) not in seen and grid[row][col] == 1:
-                    max_area = max(max_area, dfs(row,col))
+                    max_area = max(max_area, dfs(row, col))
         return max_area
         
         
