0b9aa6cad1edb53a49d4bd7c6668de3ba5fbb2e4
Hyojoon Park <hpar9758@naver.com>
2024-10-24 13:53:41 +1100

    Time: 281 ms (93.47%), Space: 21 MB (11.75%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -4,28 +4,26 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         def valid(row, col):
             return 0 <= row < m and 0 <= col < n and mat[row][col] == 1
-        
-        m = len(mat)
-        n = len(mat[0])
-        queue = deque()
+
+        m, n = len(mat), len(mat[0])
+
         seen = set()
-        
+        queue = deque()
+        distances = [[0 for _ in range(n)] for _ in range(m)]
+
         for row in range(m):
             for col in range(n):
                 if mat[row][col] == 0:
-                    queue.append((row, col, 1))
-                    seen.add((row, col))
+                    seen.add((row,col))
+                    queue.append((row,col,1))
         
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)] # right, top, left, bottom
-
+        directions = [(1,0), (0,1), (-1,0), (0,-1)] # top, right, bottom, left
         while queue:
             row, col, steps = queue.popleft()
-            
             for dx, dy in directions:
                 next_row, next_col = row + dy, col + dx
-                if (next_row, next_col) not in seen and valid(next_row, next_col):
+                if valid(next_row, next_col) and (next_row, next_col) not in seen:
                     seen.add((next_row, next_col))
                     queue.append((next_row, next_col, steps + 1))
-                    mat[next_row][next_col] = steps
-        
-        return mat
+                    distances[next_row][next_col] = steps
+        return distances
