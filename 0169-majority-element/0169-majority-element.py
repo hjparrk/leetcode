7a7bd2bd9e980b8cb71f853887805bd09bb4db1b
Hyojoon Park <hpar9758@naver.com>
2024-11-05 20:10:19 +1100

    Time: 3 ms (82.4%), Space: 18.5 MB (5.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,5 +2,6 @@ from collections import Counter
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+
         counts = Counter(nums)
-        return max(counts, key=lambda x: counts[x])
+        return max(counts, key = lambda x: counts[x])
