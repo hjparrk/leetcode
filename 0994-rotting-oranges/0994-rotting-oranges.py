106379b3b2e041730fbd6b0e4a9bc8e2cb6bf8d9
Hyojoon Park <hpar9758@naver.com>
2024-11-07 02:30:44 +1100

    Time: 7 ms (28.77%), Space: 16.7 MB (42.61%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,6 +5,7 @@ class Solution:
         m, n = len(grid), len(grid[0])
         def valid(row, col):
             return 0 <= row < m and 0 <= col < n and grid[row][col] == 1
+
         directions = [(1,0), (-1,0), (0,-1), (0,1)]
         def neighbors(node):
             nodes = []
@@ -15,33 +16,25 @@ class Solution:
                     nodes.append((new_row, new_col))
             return nodes
         
-        seen = {}
-        def bfs(root):
-            queue = deque([(root, 0)]) # node, steps
-            while queue:
-                node, steps = queue.popleft()
-                for neighbor in neighbors(node):
-                    if neighbor not in seen:
-                        seen[neighbor] = steps + 1
-                        queue.append((neighbor, steps + 1))
-                    else:
-                        if steps + 1 < seen[neighbor]:
-                            seen[neighbor] = steps + 1
-                            queue.append((neighbor, steps + 1))
-        
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == 2:
-                    bfs((row, col))
-        
-        ans = 0
+        fresh, queue = 0, deque([])
         for row in range(m):
             for col in range(n):
                 if grid[row][col] == 1:
-                    if (row, col) in seen:
-                        ans = max(ans, seen[(row, col)])
-                    else:
-                        return -1
-        return ans
+                    fresh += 1
+                if grid[row][col] == 2:
+                    queue.append((row, col))
+        if fresh == 0:
+            return 0
 
+        steps = -1
+        seen = set()
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                for neighbor in neighbors(node):
+                    if neighbor not in seen:
+                        seen.add(neighbor)
+                        queue.append(neighbor)
+            steps += 1
+        return steps if len(seen) == fresh else -1
 
