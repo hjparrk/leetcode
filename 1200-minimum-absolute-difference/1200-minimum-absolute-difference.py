2a2df6864a2e95e0bea4bea7650654dff21bb9ea
Hyojoon Park <hpar9758@naver.com>
2024-11-09 13:40:49 +1100

    Time: 43 ms (92.47%), Space: 29 MB (84.05%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -4,8 +4,10 @@ class Solution:
         min_diff, ans = float('inf'), []
         for i in range(len(arr) - 1):
             diff = arr[i + 1] - arr[i]
-            if diff < min_diff:
-                ans, min_diff = [], diff
             if diff == min_diff:
+                ans.append([arr[i], arr[i + 1]])
+            if diff < min_diff:
+                ans = []
+                min_diff = diff
                 ans.append([arr[i], arr[i + 1]])
         return ans
