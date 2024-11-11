156de0883ff331602834f7ae88206591c050d628
Hyojoon Park <hpar9758@naver.com>
2024-11-11 23:55:35 +1100

    Time: 207 ms (89.55%), Space: 18.3 MB (59.82%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -3,16 +3,22 @@ class Solution:
         total_sum = sum(nums)
         if total_sum % 2 != 0:
             return False
+        target = total_sum // 2
         
         dp = set()
         dp.add(0)
-        target = total_sum // 2
         for num in nums:
+            if num == target:
+                return True
+            if num > target:
+                return False
             next_dp = set()
             for elem in dp:
-                next_dp.add(elem)
+                if elem + num == target:
+                    return True
                 next_dp.add(elem + num)
+                next_dp.add(elem)
             dp = next_dp
-        return True if target in dp else False
+        return False
 
 
