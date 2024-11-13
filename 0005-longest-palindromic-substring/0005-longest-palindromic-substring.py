77ccafffbc08e4caeb8f87d71025133970b21c7b
Hyojoon Park <hpar9758@naver.com>
2024-11-13 22:01:44 +1100

    Time: 1733 ms (31.88%), Space: 24.5 MB (9.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1:
-            return s
-        
-        Max_Len=1
-        Max_Str=s[0]
-        s = '#' + '#'.join(s) + '#'
-        dp = [0 for _ in range(len(s))]
-        center = 0
-        right = 0
-        for i in range(len(s)):
-            if i < right:
-                dp[i] = min(right-i, dp[2*center-i])
-            while i-dp[i]-1 >= 0 and i+dp[i]+1 < len(s) and s[i-dp[i]-1] == s[i+dp[i]+1]:
-                dp[i] += 1
-            if i+dp[i] > right:
-                center = i
-                right = i+dp[i]
-            if dp[i] > Max_Len:
-                Max_Len = dp[i]
-                Max_Str = s[i-dp[i]:i+dp[i]+1].replace('#','')
-        return Max_Str
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = [0, 0]
+
+        for i in range(n):
+            dp[i][i] = True
+
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                ans = [i, i + 1]
+
+        for diff in range(2, n):
+            for i in range(n - diff):
+                j = i + diff
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    ans = [i, j]
+
+        i, j = ans
+        return s[i : j + 1]
