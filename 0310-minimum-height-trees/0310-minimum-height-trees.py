26c1a2649d9497c098217d0ffcd0a634fcb99fbd
Hyojoon Park <hpar9758@naver.com>
2024-11-15 00:43:54 +1100

    Time: 51 ms (68.18%), Space: 29.5 MB (12.31%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -2,34 +2,23 @@ from collections import defaultdict, deque
 
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if n == 1:
-            return [0]
+        if n <= 2:
+            return [i for i in range(n)]
     
-        # Build adjacency list for the graph
-        adj_list = {i: set() for i in range(n)}
-        for u, v in edges:
-            adj_list[u].add(v)
-            adj_list[v].add(u)
+
+        graph = defaultdict(set)
+        for x, y in edges:
+            graph[x].add(y)
+            graph[y].add(x)
         
-        # Find all leaf nodes (i.e., nodes with degree 1)
-        leaves = [i for i in range(n) if len(adj_list[i]) == 1]
-        
-        # Repeat until we are left with 1 or 2 nodes
+        leaves = [node for node in range(n) if len(graph[node]) == 1]
         while n > 2:
-            
-            # Remove the current leaf nodes along with their edges
             n -= len(leaves)
             new_leaves = []
-            for leaf in leaves:
-                neighbor = adj_list[leaf].pop()
-                adj_list[neighbor].remove(leaf)
-                
-                # If the neighbor becomes a new leaf node, add it to the list
-                if len(adj_list[neighbor]) == 1:
+            for node in leaves:
+                neighbor = graph[node].pop()
+                graph[neighbor].remove(node)
+                if len(graph[neighbor]) == 1:
                     new_leaves.append(neighbor)
-            
-            # Update the list of leaf nodes
             leaves = new_leaves
-        
-        # The remaining nodes are the roots of the MHTs
         return leaves
