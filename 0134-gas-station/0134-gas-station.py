c58ce546ec41e26d3c495649fccce33fc0eea16f
Hyojoon Park <hpar9758@naver.com>
2024-11-16 00:42:27 +1100

    Time: 20 ms (65.32%), Space: 22.5 MB (20.09%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,30 +1,9 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        n = len(gas)
-        diff = [0] * n
-        for i, (g, c) in enumerate(zip(gas, cost)):
-            diff[i] = g - c
-        
-        if sum(diff) < 0: return -1
-        def feasible(start, diff):
-            total = 0
-            for i in range(start, start + n):
-                total += diff[i % n]
-                if total < 0: return False
-            return True
-
-        for i in range(n):
-            if diff[i] >= 0 and feasible(i, diff):
-                return i
-        return -1
-
-        # for i, diff in enumerate(differences):
-        #     if diff >= 0:
-        #         total = 0
-        #         for j in range(i, i + len(gas)):
-        #             index = j % len(gas)
-        #             total += differences[index]
-        #             if total < 0:
-        #                 break
-        #         return i
-        return -1
+        if sum(gas) < sum(cost): return -1
+                
+        tank = index = 0
+        for i in range(len(gas)):     
+            tank += gas[i] - cost[i]
+            if tank < 0: tank, index = 0, i + 1
+        return index 
