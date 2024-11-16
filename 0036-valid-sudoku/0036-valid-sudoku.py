d705b9c31776af498d24da7c5e0be6945baf2fa8
Hyojoon Park <hpar9758@naver.com>
2024-11-16 21:15:30 +1100

    Time: 3 ms (83.07%), Space: 16.6 MB (39.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,19 @@
 from collections import defaultdict
-from pprint import pprint
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        n = len(board)
-        rows, cols = defaultdict(set), defaultdict(set)
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                cell = set()
+        n, rows, cols = len(board), defaultdict(set), defaultdict(set)
+        for i in range(0, n, 3):
+            for j in range(0, n, 3):
+                # (i, j) == top-left cell of each 3 x 3 box
+                box = set()
                 for row in range(i, i + 3):
                     for col in range(j, j + 3):
                         num = board[row][col]
                         if not num.isdigit(): continue
                         if num in rows[row] or num in cols[col]: return False
-                        if num in cell: return False
+                        if num in box: return False
                         rows[row].add(num)
                         cols[col].add(num)
-                        cell.add(num)
+                        box.add(num)
         return True
