17ebf760ca25516ff681464f92f1bcd4a236ca35
Hyojoon Park <hpar9758@naver.com>
2024-12-04 21:17:13 +1100

    Time: 3 ms (8.37%), Space: 17.8 MB (5.03%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -5,31 +5,29 @@
 #         self.left = left
 #         self.right = right
 from collections import deque
+
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
-        ans = []
-        if not root:
-            return ans
-        
-        depth = 0
+        if not root: return []
+
+        ans, level = [], 0
         queue = deque([root])
         while queue:
-            sub_ans = deque()
-            num_nodes = len(queue)
-            for _ in range(num_nodes):
+            nodes = []
+
+            size = len(queue)
+            for _ in range(size):
                 node = queue.popleft()
-                if depth % 2 == 0:
-                    sub_ans.append(node.val)
-                else:
-                    sub_ans.appendleft(node.val)
+                nodes.append(node.val)
+
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            ans.append(sub_ans)
-            depth += 1
-        
+            
+            if level % 2:
+                nodes.reverse()
+            ans.append(nodes[:])
+            level += 1
+
         return ans
-
-
