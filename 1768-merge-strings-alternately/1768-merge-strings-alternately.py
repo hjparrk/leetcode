1bf1333a4f798c8b2a71aa1737d97a88f4f7fc61
Hyojoon Park <hpar9758@naver.com>
2024-12-23 16:35:18 +1100

    Time: 33 ms (73.77%), Space: 17.9 MB (5.27%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,9 +1,14 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
+        word1, word2 = deque(word1), deque(word2)
+
         ans = ""
-        n = min(len(word1), len(word2))
-        for i in range(n):
-            ans += word1[i] + word2[i]
-        ans += (word1[n:] + word2[n:])
+        while word1 and word2:
+            ans += word1.popleft() + word2.popleft()
+        
+        if word1:
+            ans += "".join(word1)
+        if word2:
+            ans += "".join(word2)
+        
         return ans
