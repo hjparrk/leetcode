e9b7367a2b64afaeaadcae84f4cd112101221a2b
Hyojoon Park <hpar9758@naver.com>
2025-01-10 20:42:44 +1100

    Time: 43 ms (37.44%), Space: 18.3 MB (8.71%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -5,31 +5,32 @@
 #         self.left = None
 #         self.right = None
 from collections import deque
+
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+
+        # transform into undirected graph
+        parents = dict()
         def dfs(node, parent):
             if not node:
                 return None
             parents[node] = parent
             dfs(node.left, node)
             dfs(node.right, node)
-
-        parents = dict()
         dfs(root, None)
 
+        # start bfs from the target node
+        distance = 0
         seen = {target}
         queue = deque([target])
-        distance = 0
-
         while queue and distance < k:
-            length = len(queue)
-            for _ in range(length):
+            num_nodes = len(queue)
+            for _ in range(num_nodes):
                 node = queue.popleft()
                 for neighbor in [node.left, node.right, parents[node]]:
                     if neighbor and neighbor not in seen:
                         seen.add(neighbor)
                         queue.append(neighbor)
             distance += 1
-        
         return [node.val for node in queue]
         
